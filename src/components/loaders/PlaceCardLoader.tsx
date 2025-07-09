0dc6177630577effb77250e59f0b667b7313dea4
Drew Verse <drewversedesign@gmail.com>
2025-07-09 01:53:56 +0000

    const PlaceCardLoader = () => {     return (         <div className="co

--- src/components/loaders/PlaceCardLoader.tsx
+++ src/components/loaders/PlaceCardLoader.tsx
@@ -1,27 +1,16 @@
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 
 const PlaceCardLoader = () => {
     return (
-        <Card className="flex flex-col overflow-hidden">
-            <Skeleton className="w-full h-48" />
-            <CardHeader>
-                <Skeleton className="h-6 w-3/4" />
-                <Skeleton className="h-4 w-1/2" />
-            </CardHeader>
-            <CardContent className="flex-grow space-y-2">
+        <Card>
+            <Skeleton className="h-[200px] w-full" />
+            <CardContent className="space-y-3 pt-4">
                 <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-2/3" />
-                <div className="flex items-center pt-2">
-                    <Skeleton className="h-5 w-20" />
-                </div>
+                <Skeleton className="h-3 w-5/12" />
             </CardContent>
-            <CardFooter>
-                <Skeleton className="h-10 w-full" />
-            </CardFooter>
         </Card>
     )
 }
-
+ 
 export default PlaceCardLoader;
